a61e2c5ad65c9a9a2d6d958c0d24b3b0e546f73a
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2025-09-16 08:54:01 -0600

    Feat: Agregar proyecto de SpringBoot para el ahorcado

---
+++ ProyectoFinalBim4/src/test/java/com/adrianalvarez/ProyectoFinalBim4/ProyectoFinalBim4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianalvarez.ProyectoFinalBim4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalBim4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
