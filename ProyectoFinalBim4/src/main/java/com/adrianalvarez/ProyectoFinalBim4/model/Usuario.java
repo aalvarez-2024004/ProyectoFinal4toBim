7afcf80e09cdf02006a0442ac2f38cfc87f74b5e
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2025-09-16 18:12:58 -0600

    Feat: Agregar validaciones de Size y NotBlank para la entidad usuario.

--- ProyectoFinalBim4/src/main/java/com/adrianalvarez/ProyectoFinalBim4/model/Usuario.java
+++ ProyectoFinalBim4/src/main/java/com/adrianalvarez/ProyectoFinalBim4/model/Usuario.java
@@ -1,6 +1,8 @@
 package com.adrianalvarez.ProyectoFinalBim4.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name ="Usuarios")
@@ -11,9 +13,13 @@ public class Usuario {
     @Column(name = "codigo_usuario")
     private Integer codigoUsuario;
 
+    @NotBlank(message = "DEBES DE AGREGAR UN NOMBRE DE USUARIO!")
+    @Size(min = 4, max = 20, message = "El nombre de usuario debe tener minimo 4 caracteres")
     @Column(name = "usuario")
     private String usuario;
 
+    @NotBlank(message = "DEBES DE AGREGAR UNA CONTRASENA PARA EL USUARIO!")
+    @Size(min = 5, message = "La contraseña debe tener al menos 5 caracteres")
     @Column(name = "contrasena")
     private String contrasena;
 
